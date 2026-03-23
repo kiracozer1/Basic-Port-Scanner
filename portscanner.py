9608d271b9b0747e65a7a289bfd845adfe1340ad
Kıraç Özer <kiracozer@gmail.com>
2026-03-23 13:37:46 +0300

    Update portscanner.py
    I added a try-expect block because the scanning process requires information.

--- portscanner.py
+++ portscanner.py
@@ -3,11 +3,11 @@ import socket
 target_ip = input("Enter the target IP: ")
 print("Scanning ports 1 to 10000...")       
 
-for port in range(1, 10000):
+for port in range(1, 65536):
   with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sck:
        # Yeni bir TCP socket oluşturur (AF_INET = IPv4, SOCK_STREAM = TCP)
     # "with" bloğu sayesinde işlem bitince socket otomatik kapanır
-    sck.settimeout(0.5)
+    sck.settimeout(0.0000000000001)
       # Bağlantı denemesi için maksimum bekleme süresini 0.5 saniye yapar
     
 
@@ -16,7 +16,13 @@ for port in range(1, 10000):
         # Hata fırlatmaz, bunun yerine sonuç kodu döner
         # 0 dönerse bağlantı başarılı (port açık), diğer değerler başarısız
 
-    if result == 0:
-         # Eğer sonuç 0 ise yani bağlantı kurulmuşsa
-        print(f"[+] {port} is open")
+    if result == 0: # Eğer sonuç 0 ise yani bağlantı kurulmuşsa
+          try:
+                srv = socket.getservbyport(port)
+          except:
+                srv = "unknown"
+ 
+       
+          print(f"-> {port} is open|Service:{srv}")
             # Açık olan port numarasını ekrana yazdırır
+print("Scan completed")
